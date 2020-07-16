40e3c585ea4d49dca57ff4e3f549c5c813e65d19
jonhealy1 <jonathan.d.healy@gmail.com>
2020-07-16 15:20:03 -0700

    first commit

---
+++ alembic/versions/8c0bafc48570_first_migration.py
@@ -0,0 +1,24 @@
+"""First migration
+
+Revision ID: 8c0bafc48570
+Revises: 
+Create Date: 2020-07-16 20:16:37.745835
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8c0bafc48570'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
